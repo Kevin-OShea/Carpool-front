3291e141258cdc14560c4e396a86705708c0397c
Kevinoshea <kevinbarretoshea@gmail.com>
2020-07-27 16:09:24 -0400

    url with local

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,15 +1,25 @@
 
 const submitCode = function (event) {
   event.preventDefault()
-  api('a', 'b')
-    .then(response => console.log(response))
+  const params = new URLSearchParams(location.search) // location.search
+  const code = params.get('code')
+  const room = params.get('state')
+  console.log(code + ' ' + room)
+  api(code, room)
+    .then(() => console.log('worked'))
     .catch(error => console.log(error))
 }
 
-const api = function (code, roomId) {
+// http://localhost:4741/carpools/addCode
+// https://kevin-oshea.github.io/Carpool-Front/carpools/addCode
+const api = function (code, room) {
   return $.ajax({
-    method: 'GET',
-    url: 'https://intense-dawn-79586.herokuapp.com/carpools/'
+    method: 'PATCH',
+    url: 'http://localhost:4741/carpools/addCode',
+    data: {
+      code: code,
+      roomID: room
+    }
   })
 }
 module.exports = {
