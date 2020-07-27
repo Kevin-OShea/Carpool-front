1c26dc02aac0f8cba6743abe4b00563ce100cbfc
Kevinoshea <kevinbarretoshea@gmail.com>
2020-07-27 16:53:26 -0400

    tried to change

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,18 +5,12 @@ const submitCode = function (event) {
   const code = params.get('code')
   const room = params.get('state')
   console.log(code + ' ' + room)
-  // http://localhost:4741/carpools/addCode
-  // https://kevin-oshea.github.io/Carpool-Front/carpools/addCode
-  // data: {
-  //   code: code,
-  //   roomID: room
-  // }
-  api(code, room)
+  call(code, room)
     .then(() => console.log('worked'))
     .catch(error => console.log(error))
 }
 
-const api = function (code, room) {
+const call = function (code, room) {
   return $.ajax({
     method: 'GET',
     url: 'https://intense-dawn-79586.herokuapp.com/carpools/'
